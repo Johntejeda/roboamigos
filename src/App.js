93996b7d036569c30c0b2248605ca8b8beb93d03
Johntejeda <Johntejeda1969@gmail.com>
2018-08-29 17:39:07 -0500

    Search component working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Cardlist from './Cardlist.js';
 import SearchBox from './SearchBox.js'
 import {robots} from './robots.js';
 
-const App = () =>{
-  return (
-    <div className= 'tc'>
-       <h1>RoboAmigos</h1>
-       <SearchBox />
-       <Cardlist robots={robots} />
 
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state ={
+      robots: robots,
+      searchfield: ''
+    }
+  }
+  onSearchChange = (event) => {
+   this.setState({ searchfield: event.target.value })
+ }
+
+ render() {
+   const { robots, searchfield } = this.state;
+   const filteredRobots = robots.filter(robot =>{
+     return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+   })
+   return !robots.length ?
+     <h1>Loading</h1> :
+     (
+       <div className='tc'>
+         <h1 className='f1'>RoboAmigos</h1>
+         <SearchBox searchChange={this.onSearchChange}/>
+           <Cardlist robots={filteredRobots} />
+
+       </div>
+     );
+ }
 }
 
 export default App;
